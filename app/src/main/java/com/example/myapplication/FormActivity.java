f7ced407e56783094f73638a6582df1e6df14906
TG229 <simone.denti.3f@gmail.com>
2022-10-26 10:14:51 +0200

    added dialog fragment

--- app/src/main/java/com/example/myapplication/FormActivity.java
+++ app/src/main/java/com/example/myapplication/FormActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
 
     EditText editName, editSurname, editDate;
     Button saveButton;
     Person person;
+    TextView error;
     public static final String PERSON_EXTRA =
             "com.example.myapplication.Person";
 
@@ -21,6 +23,7 @@ public class FormActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
 
+        error = findViewById(R.id.error);
         editName = findViewById(R.id.editName);
         editSurname = findViewById(R.id.editSurname);
         editDate = findViewById(R.id.editDate);
@@ -31,13 +34,44 @@ public class FormActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                updatePerson();
-                Intent showResult = new Intent(FormActivity.this,
-                        ResultActivity.class);
-                showResult.putExtra(PERSON_EXTRA, person);
-                startActivity(showResult);
+                if(checkInput()) {
+                    updatePerson();
+                    Intent showResult = new Intent(FormActivity.this,
+                            ResultActivity.class);
+                    showResult.putExtra(PERSON_EXTRA, person);
+                    startActivity(showResult);
+                }
             }
         });
+    }
+    boolean checkInput(){
+        int errors = 0;
+        if(editName.getText().toString().length() == 0){
+            errors++;
+            editName.setError("Si è verificato un errore");
+        }
+        if(editSurname.getText().toString().length() == 0){
+            errors++;
+            editSurname.setError("Si è verificato un errore");
+        }
+        if(editDate.getText().toString().length() == 0){
+            errors++;
+            editDate.setError("Si è verificato un errore");
+        }
+
+        switch(errors){
+            case 0:
+                error.setVisibility(View.GONE);
+                break;
+            case 1:
+                error.setVisibility(View.VISIBLE);
+                error.setText("Si è verificato un errore.");
+                break;
+            default:
+                error.setVisibility(View.VISIBLE);
+                error.setText("Si sono verificati " + errors + " errori.");
+        }
+        return errors == 0;
     }
 
     void updatePerson(){
